c4ca3fd8f05af2c7ece36be303909e1120d020c6
bleachi <bl0222545@otc.edu>
2018-10-29 23:25:01 -0500

    added chalk text

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const chalk = require('chalk');
+
+console.log(
+    chalk.blue.bgWhite.bold('Hello Latin!\n'),
+    chalk.bgBlue('Lorem Ipsum.\n'),
+    chalk.bgMagenta.bold.underline.yellow('Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n'),
+    chalk.redBright.inverse('Praesent ornare nisl sit amet augue egestas posuere. Vivamus vestibulum ac purus a facilisis.\nMorbi elit erat, accumsan et libero in, hendrerit aliquam eros. ')
+);
